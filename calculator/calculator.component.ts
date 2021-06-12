b88daaf1a2db89fe26d70a2a71b81b6aca959deb
KothaHarika02 <60308647+KothaHarika02@users.noreply.github.com>
2021-06-12 12:10:18 +0530

    Add files via upload

---
+++ calculator/calculator.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent  {
+  myImage:string='../assets/greenary.jpg'
+  //myImage:string= 'https://explore.zoom.us/docs/image/postattendee/postattendee-groupimg.png'
+  heading:string='performing calculations';
+  applyClass:boolean=false;
+  applyStyle:boolean=true;
+  res:any
+  add(num1x:any,num2y:any){
+   this.res=Number(num1x)+Number(num2y);
+    console.log(this.res)
+}
+sub(num1x:any,num2y:any){
+  this.res=Number(num1x)+Number(num2y);
+   console.log(this.res)
+}
+mul(num1x:any,num2y:any){
+  this.res=Number(num1x)+Number(num2y);
+   console.log(this.res)
+}
+div(num1x:any,num2y:any){
+  this.res=Number(num1x)+Number(num2y);
+   console.log(this.res)
+}
+}
